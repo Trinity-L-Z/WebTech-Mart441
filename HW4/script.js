ee2ee5a7d0c3084909bed107c7717c67c4c76a90
Trinity-L-Z <trinityzachariasen@gmail.com>
2026-02-09 23:25:27 -0700

    Update script.js

---
+++ HW4/script.js
@@ -0,0 +1,11 @@
+function makeChoice(choice) {
+    if (step === 0) {
+        step = 1;
+        score +=1;
+        story.innerHTML = "Oh no! You just realized something: your favorite snack is still sitting on the counter. Do you get up? (there is also a cat sleeping on your lap)";
+        Image.src = "badbunnygaga.jpg.webp";
+    } else if (chocie === 2) {
+        step = 2;
+        story.innerHTML =
+    }
+ }}
