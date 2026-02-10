0e8eac99862a0bedc2c81b54fef50be68a8a5bf5
Trinity-L-Z <trinityzachariasen@gmail.com>
2026-02-09 23:42:28 -0700

    Update script.js

--- HW4/script.js
+++ HW4/script.js
@@ -5,10 +5,10 @@ function storyFunction(choice) {
         document.getElementById("story").innerHTML = "Your cat is sleeping very snuggly on your lap, but you just realized you left your favorite snack on the counter in the kitchen. What do you do?";
         document.getElementById("choice1").innerHTML = "Get up and pray for forgiveness";
         document.getElementById("choice2").innerHTML = "Stay put and remain unsatisfied for the halftime show";
-    } else if (choice == 2 && answer2 == "No") {
-        document.getElementById("story").innerHTML = "Pugster starts to back out, but then something in the back of Pugster's mind says 'Wait!'";
-        document.getElementById("choice1").innerHTML = "Go ahead and be brave";
-        document.getElementById("choice2").innerHTML = "Scan the situation";
+    } else if (choice == 2 && answer2 == "I'M AT THE STADIUM RARRRWW") {
+        document.getElementById("story").innerHTML = "You are impatient for the halftime show to start. How do you pass the time?";
+        document.getElementById("choice1").innerHTML = "Go get a hot dog";
+        document.getElementById("choice2").innerHTML = "Pick a fight with Benito haters";
     } else if (choice == 1 && answer1 == "Meet others") {
         document.getElementById("story").innerHTML = "Pugster walks over to a group of dogs and says hello.  Pugster is trying to decide who is genuine or not.";
         document.getElementById("choice1").innerHTML = "Evaluate all dogs";
