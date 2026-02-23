01579823d996f445f2cf2cde1c24cfde522e47a8
Trinity-L-Z <trinityzachariasen@gmail.com>
2026-02-23 11:51:39 -0700

    Update script.js

--- HW6/script.js
+++ HW6/script.js
@@ -1,17 +1,12 @@
 var imageTags = ["link1", "link2", "link3", "link", "link5", "link6"];
-// create a variable with the blank image name
 var blankImagePath = "images/link1.png";
-// create a empty array for the actual images
 var actualImages = new Array();
     
 function printBlanks()
 {
-   // call our random image creation function
     createRandomImageArray();
-    // create a for loop
     for(var i = 0; i < imageTags.length; i++)
     {
-    // iterate through the image tag ids and sets the source 
         document.getElementById(imageTags[i]).src= blankImagePath;
     }
        
@@ -21,21 +16,16 @@ function printBlanks()
 
 function createRandomImageArray()
 {
-    // create an array of actual images
     var actualImagePath = ["images/link1.png", "images/link2.png", "images/link3.png", "images/link4.png", "images/link5.png", "images/link6.png"];
-    // create another array to make sure the images only get added twice
     var count = [0,0];
-    // create a while statement to check to see if our actual image array is full
+
     while(actualImages.length < 7)
     {
-        // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-          // create an if statement that says if the total number added is less than 2, then
-        // add the image to the actual image array
+
         if(count[randomNumber] < 2)
         {
             actualImages.push(actualImagePath[randomNumber]);
-            // then add one to the array that makes sure only two images can be added
             count[randomNumber] = count[randomNumber] + 1;
         }
     }
@@ -49,8 +39,4 @@ function createRandomImageArray()
 function flipImage(number)
 {
     document.getElementById(imageTags[number]).src= actualImages[number];
-        // this should be a quick function that just changes
-        // the image based on what number was pressed
-        
-    
 }
