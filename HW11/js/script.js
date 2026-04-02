cc020dfe3a7b2f7493762b4f63d65828b8f5f6c4
Trinity-L-Z <trinityzachariasen@gmail.com>
2026-04-01 23:30:45 -0600

    Update script.js

--- HW11/js/script.js
+++ HW11/js/script.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
+
+    var i = 0;
+
             $("button").click(function () {
                 $("#musicInformation").load("data/musicInfo.json", function(responseText){
+
+                    console.log(responseText)
                     var genre = JSON.parse(responseText);
-                    $("#musicInformation").html("Name: " + genre.name 
-                + "<br>Origin Decade: " + genre.origin_decade + "<br>Parent:" + genre.parent + "<br>Subgenres:<br>" )
-                
-                /* + genre.subgenres[0] + "<br>" + genre.subgenres[1] + "<br>" + genre.subgenres[2] + "<br>" + genre.subgenres[3] + "<br>" + genre.subgenres[4]); */
+                    console.log(genre.genres);
+                    $("#musicInformation").html("Name: " + genre.genres[i].name 
+                + "<br>Origin Decade: " + genre.genres[i].origin_decade + "<br>Parent: " + genre.genres[i].parent + "<br>Subgenres:<br>" + genre.genres[i].subgenres[0] + "<br>" + genre.genres[i].subgenres[1] + "<br>" + genre.genres[i].subgenres[2] + "<br>" + genre.genres[i].subgenres[3] + "<br>" + genre.genres[i].subgenres[4]);
                 });
+      i += 1;           
             });
         });
