84580de828b99660257d6376565b06692e3b3bf9
Trinity-L-Z <trinityzachariasen@gmail.com>
2026-04-24 12:06:37 -0600

    Update script.js

--- HW15/js/script.js
+++ HW15/js/script.js
@@ -20,7 +20,9 @@ scene.add(cube);
 camera.position.z = 5;
 
 function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
 }
 animate();
