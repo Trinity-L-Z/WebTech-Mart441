60b01d79fb0a225a738bdb056ebbd91b6871855c
Trinity-L-Z <trinityzachariasen@gmail.com>
2026-04-25 19:06:23 -0600

    HW15

--- HW15/js/script.js
+++ HW15/js/script.js
@@ -2,6 +2,22 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var cube, cube2;
 var modelObject;
+var loader = new THREE.FontLoader();
+
+/*loader.load('fonts/helvetiker_regular.typeface.json', function (font) {
+var geometry = new THREE.TextGeometry('Trin Rulezz!', {
+    font: font,
+    size: 80,
+    height: 5,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 10,
+    bevelSize: 8,
+    bevelOffset: 0,
+    bevelSegments: 5
+  });
+});
+*/
 
 function createBox() {
   var geometry = new THREE.BoxGeometry();
